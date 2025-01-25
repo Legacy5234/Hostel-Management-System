bcc7bffbe26554cc8603f1fe4cd622bd325035cf
TARI SHELL OFIYOU <tarishell@yahoo.com>
2025-01-25 17:45:21 +0100

    update

--- HMS/settings.py
+++ HMS/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
 
     # CUSTOM APPS
     'a_userauthapp',
@@ -54,16 +56,8 @@ INSTALLED_APPS = [
     # THIRD PARTY PACKAGES
     'django_cleanup.apps.CleanupConfig',
     'widget_tweaks',
-    'cloudinary_storage',
-    'cloudinary',
 ]
 
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': env('CLOUD_NAME'),
-    'API_KEY': env('API_KEY'),
-    'API_SECRET': env('API_SECRET'),
-}
 
 
 MIDDLEWARE = [
@@ -156,11 +150,17 @@ STATIC_ROOT = BASE_DIR /'staticfiles'
 
 MEDIA_URL = 'media/'
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env('CLOUD_NAME'),
+    'API_KEY': env('CLOUD_API_KEY'),
+    'API_SECRET': env('CLOUD_API_SECRET')
+}
+
 if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 else:
     MEDIA_ROOT = BASE_DIR / 'media'
-    
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
